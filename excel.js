299a463be8b5115eaa45d50aada65e946fcf7701
yousrataghouti <35580687+yousrataghouti@users.noreply.github.com>
2024-05-21 17:20:45 +0200

    Update excel.js

--- excel.js
+++ excel.js
@@ -220,7 +220,7 @@
         }
 
     }
-    customElements.define("com.y.sap.sac.excel", Excel);
+    customElements.define("com-y-sap-sac-excel", Excel);
 
     // UTILS
     function loadthis(that, changedProperties) {
