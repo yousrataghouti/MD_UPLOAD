6796c3d6b89f37d0855ef6298a3bf712f8d9c1a1
yousrataghouti <35580687+yousrataghouti@users.noreply.github.com>
2024-05-22 09:54:34 +0200

    Update excel.js

--- excel.js
+++ excel.js
@@ -353,7 +353,7 @@
                                             var rec = result.split("[$@~!~@$]")[i].split("[#@~!~@#]");
                                             if (rec.length > 0) {
                                                 len = rec[0].trim().length + rec[1].trim().length + rec[2].trim().length + rec[3].trim().length + rec[4].trim().length + rec[
-                                                    5].trim().length + rec[6].trim().length + rec[7].trim().length + rec[8].trim().length;
+                                                    5].trim().length + rec[6].trim().length ;
                                                 if (len > 0) {
                                                     rec_count = rec_count + 1;
                                                     result_final.push({
